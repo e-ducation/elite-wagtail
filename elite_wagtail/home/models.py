4a7203e69087d21695418168548295e7574de3a6
weijie <wejie00@foxmail.com>
2019-03-06 12:20:00 +0800

    Add basic block

--- elite_wagtail/home/models.py
+++ elite_wagtail/home/models.py
@@ -1,7 +1,42 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.admin.edit_handlers import (
+    StreamFieldPanel,
+)
+from wagtail.documents.blocks import DocumentChooserBlock
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.embeds.blocks import EmbedBlock
 
 
 class HomePage(Page):
-    pass
+
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('text', blocks.TextBlock()),
+        ('EmailBlock', blocks.EmailBlock()),
+        ('IntegerBlock', blocks.IntegerBlock()),
+        ('FloatBlock', blocks.FloatBlock()),
+        ('DecimalBlock', blocks.DecimalBlock()),
+        ('URLBlock', blocks.URLBlock()),
+        ('BooleanBlock', blocks.BooleanBlock()),
+        ('DateBlock', blocks.DateBlock()),
+        ('TimeBlock', blocks.TimeBlock()),
+        ('DateTimeBlock', blocks.DateTimeBlock()),
+        ('RawHTMLBlock', blocks.RawHTMLBlock()),
+        ('BlockQuoteBlock', blocks.BlockQuoteBlock()),
+
+        ('ChoiceBlock', blocks.ChoiceBlock()),
+        ('PageChooserBlock', blocks.PageChooserBlock()),
+        ('DocumentChooserBlock', DocumentChooserBlock()),
+
+        ('EmbedBlock', EmbedBlock()),
+    ])
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+    ]
