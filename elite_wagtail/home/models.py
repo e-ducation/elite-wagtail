61ef8691210ecd7e8936d3e9f4a524c9d6ddff8c
jauzzz <18814128016@163.com>
2019-03-12 14:13:22 +0800

    remove subject app

--- elite_wagtail/home/models.py
+++ elite_wagtail/home/models.py
@@ -122,6 +122,10 @@ class HomePage(Page):
         ('StoryBlock', StoryBlock()),
         ('ProfessorBlock', ProfessorBlock()),
         ('CategoriesListBlock', CategoriesListBlock()),
+        ('SubjectCourse', blocks.StructBlock([
+            ('required_course', blocks.ListBlock(SeriesBlock())),
+            ('optional_course', blocks.ListBlock(SeriesBlock()))
+        ], template='home/blocks/subject_course.html')),
     ])
 
     content_panels = Page.content_panels + [
