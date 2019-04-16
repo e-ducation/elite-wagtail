77b0fd8d11a4dd92b351b1f68663a2a0a7842973
sodaling <502464760@qq.com>
2019-04-16 16:27:42 +0800

    article api

---
+++ elite_wagtail/home/app_view.py
@@ -0,0 +1,14 @@
+from wagtail.api.v2.endpoints import PagesAPIEndpoint
+from rest_framework.pagination import PageNumberPagination
+
+
+class NewPageNumberPagination(PageNumberPagination):
+    page_size_query_param = 'page_size'
+
+
+class AppPagesAPIEndpoint(PagesAPIEndpoint):
+    pagination_class = NewPageNumberPagination
+    known_query_parameters = PagesAPIEndpoint.known_query_parameters.union([
+        'page_size',
+        'page',
+    ])
